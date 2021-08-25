c1030f9cb48d8c692a3343654a13028ded5a41be
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-08-25 17:19:34 +0600

    some-update

---
+++ 23-2true-false.js
@@ -0,0 +1,28 @@
+// var name = truthy or falsey values
+
+// falsey value
+// 0;
+// "";
+// NaN;
+// null;
+// undefined;
+// false
+
+// truthy values
+// 1
+// -1
+// "dkfsa"
+// " "
+// "0"
+// []
+// {}
+// "false"
+true
+
+let name = '';
+if(name){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
---
+++ 23-4doubleVsTriple.js
@@ -0,0 +1,40 @@
+var first = 0;
+var second = "0";
+
+if(first == second){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+///////////////
+if(first === second){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+// cz triple(===) cheack the which type of value it is numberic or string
+
+
+// again---------
+
+var first1 = 0;
+var second1 = false;
+
+if(first1 == second1){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+// using triple equal
+if(first1 === second1){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+// cz triple(===) cheack the which type of value it is numeric or string
